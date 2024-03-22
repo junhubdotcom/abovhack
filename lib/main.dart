8a2a10a7e1026f31d7c23eda35edb6309a99711d
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-03-22 11:11:05 +0800

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:abovhack/SocialMedia/SocialMediaHomePage.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Financial Literacy App',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SocialMediaHomePage(),
     );
   }
 }
